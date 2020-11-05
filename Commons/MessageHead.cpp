42f43be5970cd477f11f06eb257049ae203c46c5
jasonxuwang <jasonxuwang@qq.com>
2020-11-05 20:14:52 +0800

    add buffer

--- Commons/MessageHead.cpp
+++ Commons/MessageHead.cpp
@@ -14,6 +14,7 @@ int32_t MessageHead::toBytes(char* to_buffer){
         // convert the m_Length to its byte array presentation
         //printf("m_length is : %d",m_Length);
         memcpy(to_buffer, &m_Length, sizeof(m_Length));
+        printf("buffis: %s", to_buffer);
         return sizeof(m_Length);
 }       
 
