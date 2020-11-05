f2c547fd52e96c32fdcd89a65aa41e5ccbea99a6
jasonxuwang <jasonxuwang@qq.com>
2020-11-05 15:51:44 +0800

    add

--- Protobuf/testProtobuf.cpp
+++ Protobuf/testProtobuf.cpp
@@ -7,14 +7,23 @@ int main(){
     mymsg.set_from(0);
     mymsg.set_to(1);
     const char* charstr=  "hello world!";
-    char* charstr2;
-    memcpy(charstr2, charstr, 5);
-    std::string str(charstr2);
+    std::string str(charstr);
     mymsg.set_data(str);
-
 
     std::cout << mymsg.to() << "\n";
     std::cout << mymsg.from() << "\n";
     std::cout << mymsg.data() <<  "\n";
+
+    // serialize 
+    size_t size = mymsg.ByteSizeLong(); 
+    char* buffer[1024];
+    // void *buffer = malloc(size);
+    mymsg.SerializeToArray(buffer, size);
+
+    std::cout << buffer <<  "\n";
+
+
+
+
 
 }
