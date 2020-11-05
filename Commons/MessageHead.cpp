5303fe7b5f57b3c6cb32858ba9dc79da03c607ac
jasonxuwang <jasonxuwang@qq.com>
2020-11-05 16:59:51 +0800

    add

--- Commons/MessageHead.cpp
+++ Commons/MessageHead.cpp
@@ -30,10 +30,6 @@ MessageHead::~MessageHead(){
 int32_t MessageHead::toBytes(char* to_buffer){
         // convert the m_Length to its byte array presentation
         printf("m_length is : %d",m_Length);
-        int32_t iRemainingLength = sizeof(to_buffer) - strlen(to_buffer);
-        if (sizeof(m_Length) < iRemainingLength){
-            return -1;
-        }
         memcpy(to_buffer, &m_Length, sizeof(m_Length));
         return sizeof(m_Length);
 }       
