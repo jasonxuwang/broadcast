cc2cd09e8b9ba70bae79783acd45a02b43ad8ec3
jasonxuwang <jasonxuwang@qq.com>
2020-11-06 15:06:20 +0800

    add buffer

--- Commons/TCPSocket.h
+++ Commons/TCPSocket.h
@@ -2,33 +2,7 @@
 #ifndef _TCPSOCKET_
 #define _TCPSOCKET_
 
-
-#include <stdint.h>
-#include <netinet/tcp.h>
-#include <unistd.h>
-#include <cstring>
-#include <cstdio>
-#include <iostream>
-#include <stdlib.h>
-#include <signal.h>
-#include <netinet/in.h>
-#include <sys/time.h>
-#include <sys/file.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <sys/ioctl.h>
-#include <netinet/tcp.h>
-#include <net/if.h>
-#include <dirent.h>
-#include <dlfcn.h>
-#include <sys/select.h>
-#include <assert.h>
-#include <string>
-#include <sys/epoll.h>
-#include <map>
-#include <iostream>
+#include "utils.h"
 #define BUFSIZE 1024
 
 class TCPSocket{
