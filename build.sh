6ebe52a6e34c7de45c2751d06d6d8d202650c279
jasonxuwang <jasonxuwang@qq.com>
2020-11-06 16:43:48 +0800

    remove /exe

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 git pull
+sleep 5
 g++ testClient.cpp ./Commons/*.cpp ./Client/TCPClient.cpp ./Protobuf/message.pb.cc  -o client -std=c++11 `pkg-config --cflags --libs protobuf`
 g++ testServer.cpp ./Commons/*.cpp ./Server/TCPServer.cpp ./Protobuf/message.pb.cc  -o server -std=c++11 `pkg-config --cflags --libs protobuf`
