ec0a4eb5a50d38fb3d0466caef396c1b85a2d825
jasonxuwang <jasonxuwang@qq.com>
2020-11-06 15:49:47 +0800

    add buffer

--- Commons/Serializer.cpp
+++ Commons/Serializer.cpp
@@ -1,9 +1,6 @@
 #include "Serializer.h"
 
 
-/**
- * Convert iMessageLength into its byte array form and cpy to iBuff
- */
 int32_t Serializer::encode_int32(char *iBuff,  int32_t iMessageLength) {
     char bytes[4];
     bytes[0] = (iMessageLength>>24) & 0xFF;
@@ -14,10 +11,6 @@ int32_t Serializer::encode_int32(char *iBuff,  int32_t iMessageLength) {
     return sizeof(int32_t);
 }
 
-
-/**
- * Convert the first 4 byts of iBuff to int32_t.
- */
 int32_t Serializer::decode_int32(char *iBuff) {
     int num = 0;
     for (int i=0;i<4;i++){
@@ -36,24 +29,17 @@ Serializer::~Serializer(){
 }
 
 
-/**
- * Copy iSize elements from iBuffer to m_buffer. 
- */
-
+//  Message m_Message;
+//         char m_uffer[BUFSIZ];
 int32_t Serializer::read(char* iBuffer, int32_t iSize){
       // empty buffer first 
     reset();
-    if (iSize > BUFSIZE || iSize > sizeof(iBuffer)){
-        return -1;
-    }
     memcpy(m_buffer, iBuffer, iSize);    // copy to self buffer
+   
     return 0;
 }
 
 
-/**
- * Clear m_buffer and reset offset to zero
- */
 void Serializer::reset(){
     memset(m_buffer, '\0', BUFSIZE);     // empty buffer first 
     m_MessageLength = 0;
@@ -70,16 +56,17 @@ int32_t Serializer::serialize(Message iMessage, char* iBuffer){
     //长度复制到buffer首部
     encode_int32(iBuffer,tMessageLength );
 
-    // 调用Protobuf对象方法序列化message对象
+    // serailize message对象
     iMessage.SerializeToArray(iBuffer+sizeof(int32_t), tMessageLength); // 长度放到buffer中
 
     return tMessageLength;
 						   
-} 
+} // convert message to char
+        
 
 /**
  * 在read()调用后执行。
- * 从存储的字符数组m_buffer中，解析出一个Message对象存储到成员变量m_Meesage中。
+ * 从存储的字符数组中，解析出一个Message对象存储到成员变量中。
 */   
 int32_t Serializer::deserialize(){
     // 先读长度.
@@ -93,7 +80,6 @@ int32_t Serializer::deserialize(){
     }
     m_offset += sizeof(int32_t);
 
-    // 解析Message对象
     if (!m_Message.ParseFromArray(m_buffer+m_offset, m_MessageLength) ){
         //如果读取出错，重置所有Buffer
         std::cout << "read error in deserialize" << "\n";
@@ -102,7 +88,9 @@ int32_t Serializer::deserialize(){
     }
     m_offset += m_MessageLength;
     return 1;
-}
+   
+
+}// parse a message from the current buffer, set the message member;
 
 
 
