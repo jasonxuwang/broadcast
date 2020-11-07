bb55f2411a088b2efe5b781c56f63907fc685b0f
jasonxuwang <jasonxuwang@qq.com>
2020-11-07 21:33:12 +0800

    1

--- testServer.cpp
+++ testServer.cpp
@@ -1,11 +1,16 @@
 #include "Server/TCPServer.h"
 
 
-int main(){
+int main(int argc,char **argv){
 
     TCPServer myServer;
 
     myServer.init();
+    m_TCPSocket.as_server(atoi(argv[1]));
+    m_epoll_fd  = m_epoll.epoll_init(TIMEOUT,MAXEVENT);
+    m_epoll.epoll_add(m_TCPSocket.get_socket_fd());
+    m_Serializer.reset();
+    m_Logger.setfile("/Server/server.log");
     myServer.run();
 
     return 0;
