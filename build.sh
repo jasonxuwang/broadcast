4f8f70775edfd0a1f7361f30402cfd9d369ce42b
jasonxuwang <root@localhost.localdomain>
2020-11-06 21:57:47 -0800

    recompile

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 git pull
-sleep 5
-g++ testClient.cpp ./Commons/*.cpp ./Client/TCPClient.cpp ./Protobuf/message.pb.cc  -o client -std=c++11 `pkg-config --cflags --libs protobuf`
-g++ testServer.cpp ./Commons/*.cpp ./Server/TCPServer.cpp ./Protobuf/message.pb.cc  -o server -std=c++11 `pkg-config --cflags --libs protobuf`
+sleep 3
+g++ testClient.cpp ./Commons/*.cpp ./Client/TCPClient.cpp ./Protobuf/message.pb.cc  -o cli -std=c++11 `pkg-config --cflags --libs protobuf`
+g++ testServer.cpp ./Commons/*.cpp ./Server/TCPServer.cpp ./Protobuf/message.pb.cc  -o serv -std=c++11 `pkg-config --cflags --libs protobuf`
