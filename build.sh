0b89655f9bbb42e39b3adb4448804c566e840897
jasonxuwang <jasonxuwang@qq.com>
2020-11-07 19:17:35 +0800

    1

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 git pull
-sleep 2
+sleep 1
 g++ testClient.cpp ./Commons/*.cpp ./Client/TCPClient.cpp ./Protobuf/message.pb.cc  -o cli -std=c++11 `pkg-config --cflags --libs protobuf`
 g++ testServer.cpp ./Commons/*.cpp ./Server/TCPServer.cpp ./Protobuf/message.pb.cc  -o serv -std=c++11 `pkg-config --cflags --libs protobuf`
 
