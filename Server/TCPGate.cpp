cdb6799aca59b0d052bd4dffc86a90a63cb67ea9
jasonxuwang <jasonxuwang@qq.com>
2020-11-07 22:10:10 +0800

    1

--- Server/TCPGate.cpp
+++ Server/TCPGate.cpp
@@ -34,7 +34,7 @@ void TCPGate::connect_servers(std::string conf_path){
             serv.m_sock = tsock;
             std::cout << "connected to server "  <<   serverInfos[i].ipstr<<  " " <<  serverInfos[i].port  <<"!\n";
             // add this server to servers
-            m_servers[i] = serv;
+            m_servers[tsock.get_socket_fd()] = serv;
 
             // add this socket to Epoll handler.
             m_epoll.epoll_add(tsock.get_socket_fd()); // 监听服务器的socket加入Epoll
@@ -87,8 +87,6 @@ void TCPGate::poll(){
                     continue;
                 }
                 
-                
-
                 // store this client to user map
 				Clients new_client;
 				new_client.client_id = conn_sock;
@@ -98,7 +96,6 @@ void TCPGate::poll(){
                 new_client.server_fd = m_servers[conn_sock%2].m_sock.get_socket_fd();
                 std::cout << "server assigned to this client is "<< new_client.server_fd  << "\n";
                 m_clients[conn_sock] = new_client;
-
 
 
                 // tell client he/she is connected and he/she's id; TODO
@@ -111,6 +108,65 @@ void TCPGate::poll(){
         
 		// if this is an incoming message from existing connection
 		}else{
+
+            /**
+             *如果是接受已经存在的socket发送消息，分为server 和client两部分处理
+             *  1.from server，是这个server加上了自己的信息，应该加上server id以后将其转发到所有client
+             *  2. from client, 是这个client发送了消息，应该将其转发到它对应的server
+             */if
+
+
+            // 判断是从哪里来
+            if (m_clients.find(m_epoll_event->dm_clientsata.fd) != mymap.end()) {  //如果是从client来
+                    std::cout << "[gate] msg from client " <<m_epoll_event->data.fd << "\n";  
+                    // 解包
+
+
+                    if ( recv(m_epoll_event->data.fd, m_clients[m_epoll_event->data.fd].m_recvbuf,BUFFSIZE,0) != 0) {
+
+                        std::map<int32_t, User>::iterator iter;
+                        int32_t iMessageLength;
+                        Message iMessage;
+
+                        // initialize serailizer with buffer data
+                        m_Serializer.read(m_clients[m_epoll_event->data.fd].m_recvbuf, BUFFSIZE);
+
+                        // deal with 粘包
+                        // if the serializer can decode message object from the buffer
+                        while(m_Serializer.deserialize() > 0){
+                            // 如果decode到一个message，发给server
+                            std::cout << "[gate]  Message From " << m_Serializer.m_Message.from() <<  ": "<< m_Serializer.m_Message.data() <<"\n";
+                            std::cout << "[gate]  Transform To " << m_clients[m_epoll_event->data.fd].server_fd <<"\n";
+                            
+                            
+                        }
+
+                // if recv returns 0, close the connection and unregister the user
+    			}else{
+                    std::cout << "[server] Client " << m_epoll_event->data.fd << " has left\n";
+                    if (m_epoll.epoll_close(m_epoll_event->data.fd) < 0){
+                        std::cout << "not closed properly! \n";
+                    }
+					m_epoll.epoll_close(m_epoll_event->data.fd); //?
+					m_clients.erase(m_epoll_event->data.fd);
+				}
+
+
+
+                    
+        		 
+
+            }else if(m_servers.find(m_epoll_event->data.fd) != mymap.end()){ // 如果是从server来
+                    std::cout << "[gate] msg from server " <<m_epoll_event->data.fd << "\n";  
+
+
+
+            }else{ //不是client也不是server
+                std::cout << "event come from " <<m_epoll_event->data.fd << "does not from clientor server\n";  
+            }
+
+
+
                 // clear client buffer
         		// memset( m_user_map[m_epoll_event->data.fd].m_recvbuf, '\0', BUFFSIZE );
 
