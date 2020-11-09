bd5c0dde1a78a055fb802605a28a14a549e25e45
root <root@localhost.localdomain>
2020-11-09 04:11:46 -0500

    unity network test

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ cd ./Protobuf
 protoc --cpp_out=./ message.proto
 cd ..
 
-#export LD_LIBRARY_PATH=/usr/local/lib
-#export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig
 
 
 g++ testClient.cpp ./Commons/*.cpp ./Client/TCPClient.cpp ./Protobuf/message.pb.cc  -o cli -std=c++11 `pkg-config --cflags --libs protobuf`
